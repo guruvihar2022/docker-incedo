1bf90c05bbdd12268677a705b4d7593ed341589f
unknown <guruvihar2022@gmail.com>
2023-11-09 11:34:47 +0530

    my initial project

---
+++ src/main/java/com/chandra/HelloChandra/HelloChandraApplication.java
@@ -0,0 +1,13 @@
+package com.chandra.HelloChandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloChandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloChandraApplication.class, args);
+	}
+
+}
